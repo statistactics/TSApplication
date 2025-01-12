9a0bbf992e50cea419423aa378de2677dc094a5b
calebology <calebmahweekeat@gmail.com>
2025-01-12 22:06:07 +0800

    Testing alternative priors

--- Statistical Rethinking/Exercises.R
+++ Statistical Rethinking/Exercises.R
@@ -2,14 +2,32 @@ p_grid <- seq(from = 0, to = 1, length.out = 20)
 
 # print(p_grid)
 
-prior <- rep(1, 20)
+# prior <- rep(1, 20)
 
+# Testing alternate prior 1: probability of Water cannot be less than 0.5
+prior <- ifelse(p_grid<0.5, 0, 1)
+
+# Testing alternative prior 2: probability of water
+prior <- exp(-5*abs(p_grid - 0.5))
+
+
+
+print(prior)
+
+# Evidence observed is 6W, 3L out of total of 9.
+# "Test" each probability by feeding it into the dbinom function
 likelihood <- dbinom(6, size = 9, prob = p_grid)
-# Evidence observed is 6W, 3L out of total of 9. Test each probability by feeding it into the dbinom function
 
+# Likelihood * prior (uninformative)
 unstd.posterior <- likelihood * prior
 
+print(sum(unstd.posterior))
+
+# Standardize the posterior by unstd.posterior so it sums to 1
 posterior <- unstd.posterior/sum(unstd.posterior)
+
+# Prove to myself that it sums to 1
+print(sum(posterior))
 
 plot(p_grid, posterior, type = "b", xlab = "probability of W", ylab = "posterior probability")
 mtext("20 points")
