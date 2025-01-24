455f84877ac391fe29ea87b603134130c49ea19b
calebology <calebmahweekeat@gmail.com>
2025-01-24 22:30:40 +0800

    Quick test of Q2M1

--- Statistical Rethinking/Exercises.R
+++ Statistical Rethinking/Exercises.R
@@ -1,13 +1,13 @@
-p_grid <- seq(from = 0, to = 1, length.out = 20)
+p_grid <- seq(from = 0, to = 1, length.out = 20) 
 
-# print(p_grid)
+print(p_grid)
 
-# prior <- rep(1, 20)
+# prior <- rep(1, 20) # Uninformative prior - 1 for all weights across grid
 
 # Testing alternate prior 1: probability of Water cannot be less than 0.5
-prior <- ifelse(p_grid<0.5, 0, 1)
+# prior <- ifelse(p_grid<0.5, 0, 1)
 
-# Testing alternative prior 2: probability of water
+# Testing alternative prior 2: probability of water - what does e raised to the power of the prior mean?
 prior <- exp(-5*abs(p_grid - 0.5))
 
 
@@ -16,7 +16,9 @@ print(prior)
 
 # Evidence observed is 6W, 3L out of total of 9.
 # "Test" each probability by feeding it into the dbinom function
-likelihood <- dbinom(6, size = 9, prob = p_grid)
+
+# Q2M1: 3W out of 3 tosses
+likelihood <- dbinom(3, size = 3, prob = p_grid)
 
 # Likelihood * prior (uninformative)
 unstd.posterior <- likelihood * prior
