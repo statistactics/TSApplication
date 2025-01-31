e29dd0c16c678e5d26241cdb3af1dc59b670040f
calebology <calebmahweekeat@gmail.com>
2025-01-31 19:57:52 +0800

    Added the cases from practice questions

--- Statistical Rethinking/Exercises.R
+++ Statistical Rethinking/Exercises.R
@@ -2,13 +2,13 @@ p_grid <- seq(from = 0, to = 1, length.out = 20)
 
 print(p_grid)
 
-# prior <- rep(1, 20) # Uninformative prior - 1 for all weights across grid
+prior <- rep(1, 20) # Uninformative prior - 1 for all weights across grid
 
 # Testing alternate prior 1: probability of Water cannot be less than 0.5
 # prior <- ifelse(p_grid<0.5, 0, 1)
 
 # Testing alternative prior 2: probability of water - what does e raised to the power of the prior mean?
-prior <- exp(-5*abs(p_grid - 0.5))
+# prior <- exp(-5*abs(p_grid - 0.5))
 
 
 
@@ -16,9 +16,21 @@ print(prior)
 
 # Evidence observed is 6W, 3L out of total of 9.
 # "Test" each probability by feeding it into the dbinom function
+# likelihood <- dbinom(6, size = 9, prob = p_grid)
 
-# Q2M1: 3W out of 3 tosses
-likelihood <- dbinom(3, size = 3, prob = p_grid)
+
+# Q2M1 1): 3W out of 3 tosses
+# likelihood <- dbinom(3, size = 3, prob = p_grid)
+
+# Q2M1 2): 3W out of 4 tosses
+# likelihood <- dbinom(3, size = 4, prob = p_grid)
+
+
+# Q2M1 3): 3W out of 3 tosses
+likelihood <- dbinom(5, size = 7, prob = p_grid)
+
+
+
 
 # Likelihood * prior (uninformative)
 unstd.posterior <- likelihood * prior
